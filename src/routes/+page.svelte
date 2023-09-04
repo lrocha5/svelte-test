2ec019bebf894ae89f54a63836d88a0f8073714a
Lucas Rocha <lrocha5555@gmail.com>
2023-09-04 15:09:50 -0300

    Importing font family && setting body style.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<input type="number" />
+
+<select name="" id="">
+    <option value="">Option</option>
+</select>
+
+<button>Button</button>
+
+
+
 
 <style lang="scss">
 
